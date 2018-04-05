f2bc83380abeee6ef9cab7e6b59329cbc233d193
Raimu Izumi <>
2018-04-06 01:48:57 +0900

    no message

---
+++ main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>5.12 HAPPY SUMMER WEDDING</title>
+</head>
+<body>
+
+  <?php
+  if(isset($_POST[‘name’])){
+  $name = $_POST[‘name’];
+  echo $name;
+  }
+  ?>
+
+</body>
+</html>
